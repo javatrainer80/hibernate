99dc39dade9e5f623908b4d3c243ae02eab72fe3
javatrainer80 <73056904+javatrainer80@users.noreply.github.com>
2023-07-21 15:59:40 +0530

    Hibernate: hibernate-cfg.xml,custom-cfg, hibernate.properties, java configurations
    Hibernate: hibernate-cfg.xml,custom-cfg, hibernate.properties, java configurations

--- hibernate-01/src/main/java/com/tech2java/client/Client.java
+++ hibernate-01/src/main/java/com/tech2java/client/Client.java
@@ -11,6 +11,7 @@ import com.tech2java.util.HibernateUtil5;
 
 public class Client {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		
 		
@@ -29,7 +30,7 @@ public class Client {
 			session.save(customer);
 			
 			tx.commit();
-			System.out.println("Employee object saved successfully....");
+			System.out.println("Customer object saved successfully....");
 			
 		} catch (Exception ex) {
 			System.err.println("Exception occured...."+ex.toString());
